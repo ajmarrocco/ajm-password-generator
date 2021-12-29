b98854724bdb4e2119e60f5756a635c8709ac2e6
ajmarrocco <ajmarrocco@gmail.com>
2021-12-28 20:11:37 -0500

    setting up prompts for user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,49 @@ var generatePassword = function(){
     window.alert("The numerical character of characters you entered is incorrect.  Please enter again");
     generatePassword();
   }
+
+  console.log(passwordLength);
+
+  var confirmLowercase = window.confirm("Would you like to include lowercase characters?")
+
+  if (confirmLowercase){
+    window.alert("Password will include lowercase characters");
+  }else {
+    window.alert("Password will not include lowercase characters");
+  }
+  
+  var confirmUpperCase = window.confirm("Would you like to include uppercase characters?")
+
+  if (confirmUpperCase){
+    window.alert("Password will include uppercase characters");
+  }else {
+    window.alert("Password will not include uppercase characters");
+  }
+
+  var confirmNumeric = window.confirm("Would you like to include numeric characters?")
+
+  if (confirmNumeric){
+    window.alert("Password will include numeric characters");
+  }else {
+    window.alert("Password will not include numeric characters");
+  }
+
+  var confirmSpecial = window.confirm("Would you like to include special characters? Ex: @, #, $.")
+
+  if (confirmSpecial){
+    window.alert("Password will include special characters");
+  } else {
+    window.alert("Password will not include special characters");
+  }
+
+  var confirmCriteria = window.confirm("Are you satisfied with the criteria you selected?")
+  if (confirmCriteria){
+    window.alert("Proceed");
+  } else {
+    generatePassword();
+  }
+
+  console.log(confirmSpecial);
 }
 
 
