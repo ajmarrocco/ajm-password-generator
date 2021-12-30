d598b99541a673138a365616fdfe3b6725ec555d
ajmarrocco <ajmarrocco@gmail.com>
2021-12-29 21:48:08 -0500

    generating randoms for character

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,96 +1,146 @@
 // Assignment code here
+
+//declaring arrays for characters
+var charLowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var charUpperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var charNumeric = ["0","1","2","3","4","5","6","7","8","9"];
+var charSpecial = ["@","%","+","/","!","#","$","^","?",":","(",")","}","{","[","]","`","-","_","."];
+
 var generatePassword = function(){
   //ask employee legnth of characters they want
   var passwordLength = window.prompt("Please select length of password.  Must be at least 8 characters and no more than 128 characters.  Please write the numerical value of the characters you want.  Ex: 15.");
 
-  //validate answer
+  //validate answer is between 8 and 128 charcters
   if (passwordLength >= 8 && passwordLength <= 128)
   window.alert("Proceed");
   else{
+    // if not prompt user and have them enter another value
     window.alert("The numerical character of characters you entered is incorrect.  Please enter again");
     generatePassword();
   }
 
-  console.log(passwordLength);
+  // Print passwordLength in console
+  console.log(passwordLength); //to be deleted
 
-  var confirmLowerCase = window.confirm("Would you like to include lowercase characters?")
+  // Declare lowercase variable and ask user if they want lowercase characters in password
+  var confirmLowerCase = window.confirm("Would you like to include lowercase characters?");
 
   if (confirmLowerCase){
+    //if  user wants lowercase, set confirmLowerCase to array
     window.alert("Password will include lowercase characters");
-    confirmLowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  }else {
+    confirmLowerCase = charLowerCase;
+  } else {
+    // prompt user that there will be no lowercase characters
     window.alert("Password will not include lowercase characters");
   }
   
+  // Declare upperercase variable and ask user if they want uppercase characters in password
   var confirmUpperCase = window.confirm("Would you like to include uppercase characters?")
 
   if (confirmUpperCase){
+    //if  user wants uppercase, set confirmUpperCase to array
     window.alert("Password will include uppercase characters");
-    confirmUpperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    confirmUpperCase = charUpperCase;
   }else {
+    // prompt user that there will be no uppercase characters
     window.alert("Password will not include uppercase characters");
   }
 
+  // Declare numeric variable and ask user if they want numeric characters in password
   var confirmNumeric = window.confirm("Would you like to include numeric characters?")
 
   if (confirmNumeric){
+    //if  user wants numeric, set confirmNumeric to array
     window.alert("Password will include numeric characters");
-    confirmNumeric = ["0","1","2","3","4","5","6","7","8","9"];
+    confirmNumeric = charNumeric;
   }else {
+    // prompt user that there will be no numeric characters
     window.alert("Password will not include numeric characters");
   }
 
-  
-
+  // Declare special variable and ask user if they want special characters in password
   var confirmSpecial = window.confirm("Would you like to include special characters? Ex: @, #, $.")
 
   if (confirmSpecial){
+    //if  user wants special, set confirmNSpecial to array
     window.alert("Password will include special characters");
-    confirmSpecial = ["@","%","+","/","!","#","$","^","?",":","(",")","}","{","[","]","`","-","_","."];
+    confirmSpecial = charSpecial;
   } else {
+    // prompt user that there will be no special characters
     window.alert("Password will not include special characters");
   }
 
+  // ask user to confirm if they are content with the criteria
   var confirmCriteria = window.confirm("Are you satisfied with the criteria you selected?")
   if (confirmCriteria){
     window.alert("Proceed");
   } else {
+    // run generate password code again
     generatePassword();
   }
 
-  console.log(confirmSpecial);
+  var verifyChar = function(){
+    debugger;
+    // declare character variable and set it to a random value
+    var character = Math.random();
+    console.log(character);//to be deleted
+
+    if (character > 0.75){
+      var characterChoice = confirmLowerCase;
+      debugger;
+      if (charLowerCase === characterChoice){
+        console.log(charLowerCase);
+        return;
+      } else {
+        verifyChar();
+      }
+    } else if (character < 0.75 && character > 0.5){
+      characterChoice = confirmUpperCase;
+      debugger;
+      if (charUpperCase === characterChoice){
+        console.log(charUpperCase);
+        return;
+      } else {
+        verifyChar();
+      }
+    } else if (character < 0.5 && character > 0.25){
+      characterChoice = confirmNumeric;
+      debugger;
+      if (charNumeric === characterChoice){
+        console.log(charNumeric);
+        return;
+      } else {
+        verifyChar();
+      }
+    } else{
+      characterChoice = confirmSpecial;
+      debugger;
+      if (charSpecial === characterChoice){
+        console.log(charSpecial);
+        return;
+      }else {
+        verifyChar();
+      }
+    }
+  
+    console.log(characterChoice);
+  }
+
+  debugger;
+  verifyChar();
 
   var alphabetLength = 26;
   var numberLength = 10;
   var specialLength = 20;
-
-  // for(let i = 1; i <= passwordLength; i++){
-  //   console.log(i);
-  // }
 
   for(let i = 0; i < passwordLength; i++){
     console.log(i);
     x = Math.floor(Math.random()*alphabetLength);
     console.log(confirmLowerCase[x]);
   }
-
-  // for(let i = 0; i < alphabetLength; i++){
-  //   console.log(confirmUpperCase[i]);
-  // }
-
-  // for(let i = 0; i < numberLength; i++){
-  //   console.log(confirmNumeric[i]);
-  // }
-
-  // for(let i = 0; i < specialLength; i++){
-  //   console.log(confirmSpecial[i]);
-  // }
-
-  x = Math.floor(Math.random())*alphabetLength;
-
-  
-
 }
+
+
 
 
 // Get references to the #generate element
